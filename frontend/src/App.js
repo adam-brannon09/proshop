e8a9c80bd8ea011a79cb84d3b5f2ab5cad7c59b1
adam-brannon09 <adam.brannon09@gmail.com>
2023-10-24 14:30:14 -0500

    added card component that displays products, pictures, and pricing. using products.js as a placeholder until database is built

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
 
 
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>React is working!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
