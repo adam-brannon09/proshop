3c0115634e5a85359f3c0ef7074f638e1d783ba9
adam-brannon09 <adam.brannon09@gmail.com>
2023-10-24 21:36:51 -0500

    added product screen with picture, price, description and added route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 // Create a router object
 const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       {/* Define a nested route to the HomeScreen component */}
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
